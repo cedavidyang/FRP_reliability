cac162f6cafe3db9a5e9276a9ebc19e4ae03fa13
cedavidyang <ynyang1988@yahoo.com>
2015-08-31 19:19:18 +0800

    format figs

--- postprocessing_reliability.m
+++ postprocessing_reliability.m
@@ -59,58 +59,127 @@ for i_factor = 1:nFactorFrp
     end
 end
 
-figure;
+figure1 = figure;
+axes1 = axes('Parent',figure1,'FontSize',12,'FontName','Times New Roman');
+box(axes1,'on');
+hold(axes1,'all');
+
 h1 = plot(FACTOR_FRP, mean_RE, 'MarkerFaceColor','b','Marker','o',...
           'LineWidth', 2, 'Color', 'b', 'LineStyle', '-');
-hold on
 h3 = plot(FACTOR_FRP, upper_RE, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
 plot(FACTOR_FRP, lower_RE, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
-h2 = plot(FACTOR_FRP, mean_RE+std_RE, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
-plot(FACTOR_FRP, mean_RE-std_RE, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
-plot(iFactor, betaData, '.')
-xlabel('Partial Safety Factor for FRP contribution');
-ylabel('Reliability Index   \beta');
+h2 = plot(FACTOR_FRP, mean_RE+std_RE, 'LineWidth', 1, 'Color', [0 0.498039215803146 0], 'LineStyle', '-.');
+plot(FACTOR_FRP, mean_RE-std_RE, 'LineWidth', 1, 'Color', [0 0.498039215803146 0], 'LineStyle', '-.');
+% plot(iFactor, betaData, '.')
+if strcmpi(DESIGN_CODE, 'aci')
+    xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+        'FontName','Times New Roman');
+else
+    xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+        'FontName','Times New Roman');
+end
+ylabel('Reliability index,  \beta','FontSize',12,...
+    'FontName','Times New Roman');
 legend([h1, h2, h3], 'Average reliability', '\beta \pm std', '\beta min & max');
+% Create textarrow
+annotation(figure1,'textarrow',[0.730357142857143 0.821428571428571],...
+    [0.601380952380953 0.323809523809524],'TextEdgeColor','none','FontSize',12,...
+    'FontName','Times New Roman',...
+    'String',{'\psi_f = 0.95','\beta = 3.613'});
+savefig('./figures/fig1.fig');
+print('./figures/fig1', '-depsc');
+
 figure;
 plot(FACTOR_FRP, norm_RE, 'bo-', 'linewidth', 2, 'markerfacecolor', 'b')
-xlabel('Partial Safety Factor for FRP contribution');
-ylabel('Norm of Reliability Index');
+if strcmpi(DESIGN_CODE, 'aci')
+    xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+        'FontName','Times New Roman');
+else
+    xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+        'FontName','Times New Roman');
+end
+ylabel('Norm of Reliability Index','FontSize',12,...
+    'FontName','Times New Roman');
+savefig('./figures/fig2.fig');
 
 switch SUB_TEST_DATABASE_NAME
     case {'flexure+IC', 'flexure+ic', 'flexure+rup', 'flexure+rupture'}
         figure;
+        figure1 = figure;
+        axes1 = axes('Parent',figure1,'FontSize',12,'FontName','Times New Roman');
+        box(axes1,'on');
+        hold(axes1,'all');
         h1 = plot(FACTOR_FRP, mean_RE_ductile, 'MarkerFaceColor','b','Marker','o',...
             'LineWidth', 2, 'Color', 'b', 'LineStyle', '-');
-        hold on
         h3 = plot(FACTOR_FRP, upper_RE_ductile, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
         plot(FACTOR_FRP, lower_RE_ductile, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
-        h2 = plot(FACTOR_FRP, mean_RE_ductile+std_RE_ductile, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
-        plot(FACTOR_FRP, mean_RE_ductile-std_RE_ductile, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
+        h2 = plot(FACTOR_FRP, mean_RE_ductile+std_RE_ductile, 'LineWidth', 1, 'Color',[0 0.498039215803146 0], 'LineStyle', '-.');
+        plot(FACTOR_FRP, mean_RE_ductile-std_RE_ductile, 'LineWidth', 1, 'Color', [0 0.498039215803146 0], 'LineStyle', '-.');
         xlabel('Partial Safety Factor for FRP contribution');
-        ylabel('Reliability Index   \beta');
+        if strcmpi(DESIGN_CODE, 'aci')
+            xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        else
+            xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        end
+        ylabel('Reliability index,  \beta','FontSize',12,...
+            'FontName','Times New Roman');
         legend([h1, h2, h3], 'Average reliability', '\beta \pm std', '\beta min & max');
+        % Create textarrow
+        annotation(figure1,'textarrow',[0.730357142857143 0.821428571428571],...
+            [0.601380952380953 0.323809523809524],'TextEdgeColor','none','FontSize',12,...
+            'FontName','Times New Roman',...
+            'String',{'\psi_f = 0.95','\beta = 3.613'});
+        savefig('./figures/fig11.fig');
+        print('./figures/fig11', '-depsc');
+        
         figure;
         plot(FACTOR_FRP, norm_RE_ductile, 'bo-', 'linewidth', 2, 'markerfacecolor', 'b')
-        xlabel('Partial Safety Factor for FRP contribution');
-        ylabel('Norm of Reliability Index');
+        if strcmpi(DESIGN_CODE, 'aci')
+            xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        else
+            xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        end
+        ylabel('Norm of Reliability Index','FontSize',12,...
+            'FontName','Times New Roman');
         hold on
         plot(FACTOR_FRP, norm_RE_brittle, 'go-', 'linewidth', 2, 'markerfacecolor', 'g')
+        savefig('./figures/fig21.fig');
 
-        figure;
+        figure1 = figure;
+        axes1 = axes('Parent',figure1,'FontSize',12,'FontName','Times New Roman');
+        box(axes1,'on');
+        hold(axes1,'all');
         h1 = plot(FACTOR_FRP, mean_RE_brittle, 'MarkerFaceColor','b','Marker','o',...
             'LineWidth', 2, 'Color', 'b', 'LineStyle', '-');
-        hold on
         h3 = plot(FACTOR_FRP, upper_RE_brittle, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
         plot(FACTOR_FRP, lower_RE_brittle, 'LineWidth', 1, 'Color', 'r', 'LineStyle', '--');
-        h2 = plot(FACTOR_FRP, mean_RE_brittle+std_RE_brittle, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
-        plot(FACTOR_FRP, mean_RE_brittle-std_RE_brittle, 'LineWidth', 1, 'Color', 'g', 'LineStyle', '-.');
-        xlabel('Partial Safety Factor for FRP contribution');
-        ylabel('Reliability Index   \beta');
+        h2 = plot(FACTOR_FRP, mean_RE_brittle+std_RE_brittle, 'LineWidth', 1, 'Color', [0 0.498039215803146 0], 'LineStyle', '-.');
+        plot(FACTOR_FRP, mean_RE_brittle-std_RE_brittle, 'LineWidth', 1, 'Color', [0 0.498039215803146 0], 'LineStyle', '-.');
+        if strcmpi(DESIGN_CODE, 'aci')
+            xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        else
+            xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        end
+        ylabel('Reliability index,  \beta','FontSize',12,...
+            'FontName','Times New Roman');
         legend([h1, h2, h3], 'Average reliability', '\beta \pm std', '\beta min & max');        
         figure;
         plot(FACTOR_FRP, norm_RE_brittle, 'bo-', 'linewidth', 2, 'markerfacecolor', 'b')
-        xlabel('Partial Safety Factor for FRP contribution');
-        ylabel('Norm of Reliability Index');
+        if strcmpi(DESIGN_CODE, 'aci')
+            xlabel('Strength reduction factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        else
+            xlabel('Partial safety factor for FRP contribution','FontSize',12,...
+                'FontName','Times New Roman');
+        end
+        ylabel('Norm of Reliability Index','FontSize',12,...
+            'FontName','Times New Roman');
         hold on
         plot(FACTOR_FRP, norm_RE_brittle, 'go-', 'linewidth', 2, 'markerfacecolor', 'g')
     otherwise
@@ -143,6 +212,7 @@ end
 %% cov of resistance
 figure;
 plot(resistStd./resistMean*100, 'o');
+savefig('./figures/fig3.fig');
 
 % convergMean = zeros(N_MC, 1);
 % convergStd = zeros(N_MC, 1);
