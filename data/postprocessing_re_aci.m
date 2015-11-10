7c1a5c923b1eb5636a8eb2d1527f5833ea31f7b3
cedavidyang <ynyang1988@yahoo.com>
2015-11-10 14:10:12 +0800

    add postprocessing of flexural strengthening

--- data/postprocessing_re_aci.m
+++ data/postprocessing_re_aci.m
@@ -1,6 +1,7 @@
 clear;
 MAX_YR = 50;
 code = input('design code\n', 's');
+frp_form = input('frp for shear or flexure: 1 for shear, 2 for flexure\n');
 switch lower(code)
     case {'hk','tr'}
         FACTOR_FRP = (1.00:0.05:2.00)';
@@ -23,41 +24,78 @@ switch fc_type
         fc = 'cov10'; fccov=0.1;
     otherwise
 end
-struct_s = load(strcat('data_', code, '_shear+side_', fc, '_', num2str(fccov), '.mat'));
-struct_u = load(strcat('data_', code, '_shear+U_', fc, '_', num2str(fccov), '.mat'));
-struct_w = load(strcat('data_', code, '_shear+W_', fc, '_', num2str(fccov), '.mat'));
-life_s = 50*log(normcdf(beta_T_50))./log(normcdf(struct_s.mean_RE)); life_s(life_s>MAX_YR) = MAX_YR;
-life_u = 50*log(normcdf(beta_T_50))./log(normcdf(struct_u.mean_RE)); life_u(life_u>MAX_YR) = MAX_YR;
-life_w = 50*log(normcdf(beta_T_50))./log(normcdf(struct_w.mean_RE)); life_w(life_w>MAX_YR) = MAX_YR;
-
-%% post processing of reliability analysis
-figure1 = figure;
-axes1 = axes('Parent',figure1,'FontSize',8,'FontName','Times New Roman');
-box(axes1,'on');
-hold(axes1,'all');
-
-hs1 = plot(FACTOR_FRP, struct_s.mean_RE, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-hu1 = plot(FACTOR_FRP, struct_u.mean_RE, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-hw1 = plot(FACTOR_FRP, struct_w.mean_RE, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-
-xtxt1 = xlabel('FRP strength reduction factor','FontSize',8,...
-    'FontName','Times New Roman', 'Interpreter','latex');
-ytxt1 = ylabel('Mean reliability index, $\bar{\beta}$','FontSize',8,...
-    'FontName','Times New Roman', 'Interpreter','latex');
-
-figure2 = figure;
-axes2 = axes('Parent',figure2,'FontSize',8,'FontName','Times New Roman');
-box(axes2,'on');
-hold(axes2,'all');
-
-hs2 = plot(FACTOR_FRP, life_s, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-hu2 = plot(FACTOR_FRP, life_u, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-hw2 = plot(FACTOR_FRP, life_w, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-
-xtxt2 = xlabel('FRP strength reduction factor','FontSize',8,...
-    'FontName','Times New Roman', 'Interpreter','latex');
-ytxt2 = ylabel('Deisgn sevice life after strengthening (year)','FontSize',8,...
-    'FontName','Times New Roman', 'Interpreter','latex');
+switch frp_form
+    case 1
+        beam = 'shear';
+        struct_s = load(strcat('data_', code, '_', beam, '+side_', fc, '_', num2str(fccov), '.mat'));
+        struct_u = load(strcat('data_', code, '_', beam, '+U_', fc, '_', num2str(fccov), '.mat'));
+        struct_w = load(strcat('data_', code, '_', beam', '+W_', fc, '_', num2str(fccov), '.mat'));
+        life_s = 50*log(normcdf(beta_T_50))./log(normcdf(struct_s.mean_RE)); life_s(life_s>MAX_YR) = MAX_YR;
+        life_u = 50*log(normcdf(beta_T_50))./log(normcdf(struct_u.mean_RE)); life_u(life_u>MAX_YR) = MAX_YR;
+        life_w = 50*log(normcdf(beta_T_50))./log(normcdf(struct_w.mean_RE)); life_w(life_w>MAX_YR) = MAX_YR;
+        
+        %% post processing of reliability analysis
+        figure1 = figure;
+        axes1 = axes('Parent',figure1,'FontSize',8,'FontName','Times New Roman');
+        box(axes1,'on');
+        hold(axes1,'all');
+        
+        hs1 = plot(FACTOR_FRP, struct_s.mean_RE, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hu1 = plot(FACTOR_FRP, struct_u.mean_RE, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hw1 = plot(FACTOR_FRP, struct_w.mean_RE, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        
+        xtxt1 = xlabel('FRP strength reduction factor','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        ytxt1 = ylabel('Mean reliability index, $\bar{\beta}$','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        
+        figure2 = figure;
+        axes2 = axes('Parent',figure2,'FontSize',8,'FontName','Times New Roman');
+        box(axes2,'on');
+        hold(axes2,'all');
+        
+        hs2 = plot(FACTOR_FRP, life_s, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hu2 = plot(FACTOR_FRP, life_u, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hw2 = plot(FACTOR_FRP, life_w, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        
+        xtxt2 = xlabel('FRP strength reduction factor','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        ytxt2 = ylabel('Deisgn sevice life after strengthening (year)','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+    case 2
+        beam = 'flexure';
+        struct_ic = load(strcat('data_', code, '_', beam, '+IC_', fc, '_', num2str(fccov), '.mat'));
+        struct_rup = load(strcat('data_', code, '_', beam, '+rupture_', fc, '_', num2str(fccov), '.mat'));
+        life_ic = 50*log(normcdf(beta_T_50))./log(normcdf(struct_ic.mean_RE)); life_ic(life_ic>MAX_YR) = MAX_YR;
+        life_rup = 50*log(normcdf(beta_T_50))./log(normcdf(struct_rup.mean_RE)); life_rup(life_rup>MAX_YR) = MAX_YR;
+        
+        %% post processing of reliability analysis
+        figure1 = figure;
+        axes1 = axes('Parent',figure1,'FontSize',8,'FontName','Times New Roman');
+        box(axes1,'on');
+        hold(axes1,'all');
+        
+        hic1 = plot(FACTOR_FRP, struct_ic.mean_RE, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hrup1 = plot(FACTOR_FRP, struct_rup.mean_RE, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        
+        xtxt1 = xlabel('FRP strength reduction factor','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        ytxt1 = ylabel('Mean reliability index, $\bar{\beta}$','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        
+        figure2 = figure;
+        axes2 = axes('Parent',figure2,'FontSize',8,'FontName','Times New Roman');
+        box(axes2,'on');
+        hold(axes2,'all');
+        
+        hic2 = plot(FACTOR_FRP, life_ic, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        hrup2 = plot(FACTOR_FRP, life_rup, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
+        
+        xtxt2 = xlabel('FRP strength reduction factor','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+        ytxt2 = ylabel('Deisgn sevice life after strengthening (year)','FontSize',8,...
+            'FontName','Times New Roman', 'Interpreter','latex');
+end     
 
 %% change the following properties from figure inspector
 set(figure1, 'Units','inches', 'Position', [0, 0, 3.5, 2.625]);
