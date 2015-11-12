bdda68ed89af15c9d5310aabc74049b3946504b6
cedavidyang <ynyang1988@yahoo.com>
2015-11-12 11:01:09 +0800

    revision of re postprocessing to add aci models

---
+++ data/get_re_data.m
@@ -0,0 +1,37 @@
+function [re_smp, mean_RE, std_RE, upper_RE, lower_RE, norm_RE_yr30, ...
+    norm_RE_yr40, norm_RE_yr50] = get_re_data( data_struct, psi_f, code, beta_T_50)
+% get re and summaries from data
+npsi = length(psi_f);
+beta_T_30 = life2re(30, beta_T_50);
+beta_T_40 = life2re(40, beta_T_50);
+switch lower(code)
+    case {'aci', 'acinew'}
+        for ipsi = 1:npsi
+            re_smp = data_struct.re_data{1}(:, ipsi, :);
+            re_smp = re_smp(:);
+            re_smp( isnan(re_smp) ) = [];
+            norm_RE_yr30(ipsi) = mean((re_smp-beta_T_30).^2);
+            norm_RE_yr40(ipsi) = mean((re_smp-beta_T_40).^2);
+            norm_RE_yr50(ipsi) = mean((re_smp-beta_T_50).^2);
+            mean_RE(ipsi) = mean(re_smp);
+            std_RE(ipsi) = std(re_smp);
+            upper_RE(ipsi) = max(re_smp);
+            lower_RE(ipsi) = min(re_smp);
+        end
+    otherwise
+        for ipsi = 1:npsi
+            re_smp = data_struct.re_data{ipsi}(:, 1, :);
+            re_smp = re_smp(:);
+            re_smp( isnan(re_smp) ) = [];
+            norm_RE_yr30(ipsi) = mean((re_smp-beta_T_30).^2);
+            norm_RE_yr40(ipsi) = mean((re_smp-beta_T_40).^2);
+            norm_RE_yr50(ipsi) = mean((re_smp-beta_T_50).^2);
+            mean_RE(ipsi) = mean(re_smp);
+            std_RE(ipsi) = std(re_smp);
+            upper_RE(ipsi) = max(re_smp);
+            lower_RE(ipsi) = min(re_smp);
+        end
+end
+
+return
+end
--- data/postprocessing_re.m
+++ data/postprocessing_re.m
@@ -1,19 +1,16 @@
 clear;
-MAX_YR = 50;
+
 code = input('design code\n', 's');
 frp_form = input('frp for shear or flexure: 1 for shear, 2 for flexure\n');
 switch lower(code)
     case {'aci', 'acinew'}
-        psi_f = 1.00;
-        FACTOR_FRP = (0.50:0.05:1.00)';
+        psi_f = (0.10:0.05:2.00)';
         beta_T_50 = 3.5;
     case {'hk', 'tr'}
         psi_f = (0.10:0.05:2.00)';
-        FACTOR_FRP = 1.00;
         beta_T_50 = 3.8;
     case {'gb'}
         psi_f = (0.10:0.05:2.00)';
-        FACTOR_FRP = 1.00;
         beta_T_50 = 3.2;    
     otherwise
 end
@@ -27,62 +24,39 @@ switch fc_type
         fc = 'cov10'; fccov=0.1;
     otherwise
 end
-beta_T_30 = life2re(30, beta_T_50);
-beta_T_40 = life2re(40, beta_T_50);
+
 switch frp_form
     case 1
-        beam = 'shear';
         struct_s = load(strcat(code, '_', 'shear', '+side_', fc, num2str(fccov), '.mat'));
         struct_u = load(strcat(code, '_', 'shear', '+U_', fc, num2str(fccov), '.mat'));
         struct_w = load(strcat(code, '_', 'shear', '+W_', fc, num2str(fccov), '.mat'));
-        
-        npsi = length(psi_f);      
-        for ipsi = 1:length(psi_f)            
-            % side bonding
-            s_RE_col = struct_s.re_data{ipsi}(:, 1, :);
-            s_RE_col = s_RE_col(:);
-            s_RE_col( isnan(s_RE_col) ) = [];
-            norm_RE_s_yr30(ipsi) = mean((s_RE_col-beta_T_30).^2);
-            norm_RE_s_yr40(ipsi) = mean((s_RE_col-beta_T_40).^2);
-            norm_RE_s_yr50(ipsi) = mean((s_RE_col-beta_T_50).^2);
-            mean_RE_s(ipsi) = mean(s_RE_col);
-            std_RE_s(ipsi) = std(s_RE_col);
-            upper_RE_s(ipsi) = max(s_RE_col);
-            lower_RE_s(ipsi) = min(s_RE_col);
-            
-            % U jacketing
-            u_RE_col = struct_u.re_data{ipsi}(:, 1, :);
-            u_RE_col = u_RE_col(:);
-            u_RE_col( isnan(u_RE_col) ) = [];
-            norm_RE_u_yr30(ipsi) = mean((u_RE_col-beta_T_30).^2);
-            norm_RE_u_yr40(ipsi) = mean((u_RE_col-beta_T_40).^2);
-            norm_RE_u_yr50(ipsi) = mean((u_RE_col-beta_T_50).^2);
-            mean_RE_u(ipsi) = mean(u_RE_col);
-            std_RE_u(ipsi) = std(u_RE_col);
-            upper_RE_u(ipsi) = max(u_RE_col);
-            lower_RE_u(ipsi) = min(u_RE_col);
-            
-            % complete wrapping
-            w_RE_col = struct_w.re_data{ipsi}(:, 1, :);
-            w_RE_col = w_RE_col(:);
-            w_RE_col( isnan(w_RE_col) ) = [];
-            norm_RE_w_yr30(ipsi) = mean((w_RE_col-beta_T_30).^2);
-            norm_RE_w_yr40(ipsi) = mean((w_RE_col-beta_T_40).^2);
-            norm_RE_w_yr50(ipsi) = mean((w_RE_col-beta_T_50).^2);
-            mean_RE_w(ipsi) = mean(w_RE_col);
-            std_RE_w(ipsi) = std(w_RE_col);
-            upper_RE_w(ipsi) = max(w_RE_col);
-            lower_RE_w(ipsi) = min(w_RE_col);
+
+        data_struct_array = {struct_s, struct_u, struct_w}';
+        smp_RE = {};
+        mean_RE = {};
+        std_RE = {};
+        upper_RE = {};
+        lower_RE = {};
+        norm_RE_yr30 = {};
+        norm_RE_yr40 = {};
+        norm_RE_yr50 = {};
+
+        for idata = 1:3
+            [re_smpi, mean_REi, std_REi, upper_REi, lower_REi, norm_RE_yr30i, ...
+                norm_RE_yr40i, norm_RE_yr50i] = get_re_data( ...
+                data_struct_array{idata}, psi_f, code, beta_T_50);
+            smp_RE{end+1} = re_smpi;
+            mean_RE{end+1} = mean_REi;
+            std_RE{end+1} = std_REi;
+            upper_RE{end+1} = upper_REi;
+            lower_RE{end+1} = lower_REi;
+            norm_RE_yr30{end+1} = norm_RE_yr30i;
+            norm_RE_yr40{end+1} = norm_RE_yr40i;
+            norm_RE_yr50{end+1} = norm_RE_yr50i;
         end
 
         %% post processing of shear
         figs = {}; axs = {};
-        mean_RE = {mean_RE_s, mean_RE_u, mean_RE_w};
-        upper_RE = {upper_RE_s, upper_RE_u, upper_RE_w};
-        lower_RE = {lower_RE_s, lower_RE_u, lower_RE_w};
-        norm_RE_yr30 = {norm_RE_s_yr30, norm_RE_u_yr30, norm_RE_w_yr30};
-        norm_RE_yr40 = {norm_RE_s_yr40, norm_RE_u_yr40, norm_RE_w_yr40};
-        norm_RE_yr50 = {norm_RE_s_yr50, norm_RE_u_yr50, norm_RE_w_yr50};
         xlim_cell = {[0.1,2.0], [0.1,2.0], [0.1, 2.0]};
         for ifig = 1:3
             figs{end+1} = figure;
@@ -119,45 +93,33 @@ switch frp_form
     case 2
         struct_ic = load(strcat(code, '_', 'flexure', '+IC_', fc, num2str(fccov), '.mat'));
         struct_rup = load(strcat(code, '_', 'flexure', '+rupture_', fc, num2str(fccov), '.mat'));
-        psi_f = struct_ic.psi_f;
         
-        npsi = length(psi_f);
+        data_struct_array = {struct_ic, struct_rup}';
+        smp_RE = {};
+        mean_RE = {};
+        std_RE = {};
+        upper_RE = {};
+        lower_RE = {};
+        norm_RE_yr30 = {};
+        norm_RE_yr40 = {};
+        norm_RE_yr50 = {};
         
-        for ipsi = 1:length(psi_f)            
-         
-            % IC debonding
-            ic_RE_col = struct_ic.re_data{ipsi}(:, 1, :);
-            ic_RE_col = ic_RE_col(:);
-            ic_RE_col( isnan(ic_RE_col) ) = [];
-            norm_RE_ic_yr30(ipsi) = mean((ic_RE_col-beta_T_30).^2);
-            norm_RE_ic_yr40(ipsi) = mean((ic_RE_col-beta_T_40).^2);
-            norm_RE_ic_yr50(ipsi) = mean((ic_RE_col-beta_T_50).^2);
-            mean_RE_ic(ipsi) = mean(ic_RE_col);
-            std_RE_ic(ipsi) = std(ic_RE_col);
-            upper_RE_ic(ipsi) = max(ic_RE_col);
-            lower_RE_ic(ipsi) = min(ic_RE_col);
-            
-            % FRP rupture
-            rup_RE_col = struct_rup.re_data{ipsi}(:, 1, :);
-            rup_RE_col = rup_RE_col(:);
-            rup_RE_col( isnan(rup_RE_col) ) = [];
-            norm_RE_rup_yr30(ipsi) = mean((rup_RE_col-beta_T_30).^2);
-            norm_RE_rup_yr40(ipsi) = mean((rup_RE_col-beta_T_40).^2);
-            norm_RE_rup_yr50(ipsi) = mean((rup_RE_col-beta_T_50).^2);
-            mean_RE_rup(ipsi) = mean(rup_RE_col);
-            std_RE_rup(ipsi) = std(rup_RE_col);
-            upper_RE_rup(ipsi) = max(rup_RE_col);
-            lower_RE_rup(ipsi) = min(rup_RE_col);
+        for idata = 1:2
+            [re_smpi, mean_REi, std_REi, upper_REi, lower_REi, norm_RE_yr30i, ...
+                norm_RE_yr40i, norm_RE_yr50i] = get_re_data( ...
+                data_struct_array{idata}, FACTOR_FRP, code, beta_T_50);
+            smp_RE{end+1} = re_smpi;
+            mean_RE{end+1} = mean_REi;
+            std_RE{end+1} = std_REi;
+            upper_RE{end+1} = upper_REi;
+            lower_RE{end+1} = lower_REi;
+            norm_RE_yr30{end+1} = norm_RE_yr30i;
+            norm_RE_yr40{end+1} = norm_RE_yr40i;
+            norm_RE_yr50{end+1} = norm_RE_yr50i;
         end
 
         %% post processing of shear
         figs = {}; axs = {};
-        mean_RE = {mean_RE_ic, mean_RE_rup};
-        upper_RE = {upper_RE_ic, upper_RE_rup};
-        lower_RE = {lower_RE_ic, lower_RE_rup};
-        norm_RE_yr30 = {norm_RE_ic_yr30, norm_RE_rup_yr30};
-        norm_RE_yr40 = {norm_RE_ic_yr40, norm_RE_rup_yr40};
-        norm_RE_yr50 = {norm_RE_ic_yr50, norm_RE_rup_yr50};
         xlim_cell = {[0.1,2.0], [0.1,2.0]};
         for ifig = 1:2
             figs{end+1} = figure;
