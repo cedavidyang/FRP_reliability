b3d75fce231952f687d362d765dc9d3467002f28
cedavidyang <ynyang1988@yahoo.com>
2015-11-14 13:17:14 +0800

    update cov cmp

--- data/postprocessing_cov.m
+++ data/postprocessing_cov.m
@@ -5,38 +5,34 @@ psif_s = 0.70; psif_u = 1.0; psif_w = 1.0;
 code = input('design code\n', 's');
 switch lower(code)
     case {'hk','tr'}
+        psi_f = (0.1:0.05:2.0)';
         FACTOR_FRP = (1.00:0.05:2.00)';
         beta_T_50=3.8;
     case 'gb'
-        FACTOR_FRP = (1.00:0.05:2.00)';
+        psi_f = (0.1:0.05:2.0)';
+        FACTOR_FRP = 1.00;
         beta_T_50=3.2;
     case{'aci', 'acinew'}
-        FACTOR_FRP = (0.50:0.05:1.00)';
+        psi_f = 1.00;
+        FACTOR_FRP = (0.1:0.05:2.0)';
         beta_T_50=3.5;
     otherwise
 end
-% scheme_num = input('strengthening scheme: 1 shear+side, 2 shear+U, 3 shear+W\n', 's');
-% switch scheme
-%     case 1
-%         scheme = 'shear+side';
-%     case 2
-%         scheme = 'shear+U';
-%     case 3
-%         scheme = 'shear+W';
-%     otherwise
-% end   
 ncov = length(fccov);
 re_s = zeros(ncov,1);
 re_u = zeros(ncov,1);
 re_w = zeros(ncov,1);
 for icov=1:ncov 
     cov = fccov(icov);
-    struct_s = load(strcat('data_', code, '_', 'shear+side', '_cov', num2str(100*cov), '_', num2str(cov), '.mat'));
-    struct_u = load(strcat('data_', code, '_', 'shear+U', '_cov', num2str(100*cov), '_', num2str(cov), '.mat'));
-    struct_w = load(strcat('data_', code, '_', 'shear+W', '_cov', num2str(100*cov), '_', num2str(cov), '.mat'));
-    re_s(icov) = struct_s.mean_RE(FACTOR_FRP==psif_s);
-    re_u(icov) = struct_u.mean_RE(FACTOR_FRP==psif_u);
-    re_w(icov) = struct_w.mean_RE(FACTOR_FRP==psif_w);
+    struct_s = load(strcat(code, '_', 'shear+side', '_fccov.mat'));
+    struct_u = load(strcat(code, '_', 'shear+U', '_fccov.mat'));
+    struct_w = load(strcat(code, '_', 'shear+W', '_fccov.mat'));
+    re_s_smp = struct_s.re_data{icov}(:,FACTOR_FRP==psif_side,:); re_s_smp = re_s_smp(:);
+    re_s(icov) = mean(re_s_smp);
+    re_u_smp = struct_u.re_data{icov}(:,FACTOR_FRP==psif_u,:); re_u_smp = re_u_smp(:);
+    re_u(icov) = mean(re_u_smp);
+    re_w_smp = struct_w.re_data{icov}(:,FACTOR_FRP==psif_w,:); re_w_smp = re_w_smp(:);
+    re_w(icov) = mean(re_w_smp);
 end
 
 %% post processing of reliability analysis
