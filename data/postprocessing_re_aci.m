8f1516d54c25c5cebf11e4f21b7de6ba2a41363c
cedavidyang <ynyang1988@yahoo.com>
2015-11-16 11:32:25 +0800

    aci revision postprocessing of beta

--- data/postprocessing_re_aci.m
+++ data/postprocessing_re_aci.m
@@ -1,19 +1,10 @@
 clear;
-MAX_YR = 50;
-code = input('design code\n', 's');
+
 frp_form = input('frp for shear or flexure: 1 for shear, 2 for flexure\n');
-switch lower(code)
-    case {'hk','tr'}
-        FACTOR_FRP = (1.00:0.05:2.00)';
-        beta_T_50=3.8;
-    case 'gb'
-        FACTOR_FRP = (1.00:0.05:2.00)';
-        beta_T_50=3.2;
-    case{'aci', 'acinew'}
-        FACTOR_FRP = (0.50:0.05:1.00)';
-        beta_T_50=3.5;
-    otherwise
-end
+code = input('design code\n', 's');
+psi_f = (0.10:0.05:2.00)';
+beta_T = [3.0, 3.2, 3.5, 3.7];
+
 fc_type = input('fc type: 1 for fcdet, 2 for 125 bias, 3 for cov10\n');
 switch fc_type
     case 1
@@ -22,90 +13,160 @@ switch fc_type
         fc = 'fc125'; fccov=0.2;
     case 3
         fc = 'cov10'; fccov=0.1;
+    case 5
+        fc = 'cov15'; fccov=0.15;    
     otherwise
 end
+
 switch frp_form
     case 1
-        beam = 'shear';
-        struct_s = load(strcat('data_', code, '_', beam, '+side_', fc, '_', num2str(fccov), '.mat'));
-        struct_u = load(strcat('data_', code, '_', beam, '+U_', fc, '_', num2str(fccov), '.mat'));
-        struct_w = load(strcat('data_', code, '_', beam', '+W_', fc, '_', num2str(fccov), '.mat'));
-        life_s = 50*log(normcdf(beta_T_50))./log(normcdf(struct_s.mean_RE)); life_s(life_s>MAX_YR) = MAX_YR;
-        life_u = 50*log(normcdf(beta_T_50))./log(normcdf(struct_u.mean_RE)); life_u(life_u>MAX_YR) = MAX_YR;
-        life_w = 50*log(normcdf(beta_T_50))./log(normcdf(struct_w.mean_RE)); life_w(life_w>MAX_YR) = MAX_YR;
-        
-        %% post processing of reliability analysis
-        figure1 = figure;
-        axes1 = axes('Parent',figure1,'FontSize',8,'FontName','Times New Roman');
-        box(axes1,'on');
-        hold(axes1,'all');
-        
-        hs1 = plot(FACTOR_FRP, struct_s.mean_RE, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hu1 = plot(FACTOR_FRP, struct_u.mean_RE, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hw1 = plot(FACTOR_FRP, struct_w.mean_RE, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        
-        xtxt1 = xlabel('FRP strength reduction factor','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        ytxt1 = ylabel('Mean reliability index, $\bar{\beta}$','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        
-        figure2 = figure;
-        axes2 = axes('Parent',figure2,'FontSize',8,'FontName','Times New Roman');
-        box(axes2,'on');
-        hold(axes2,'all');
-        
-        hs2 = plot(FACTOR_FRP, life_s, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hu2 = plot(FACTOR_FRP, life_u, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hw2 = plot(FACTOR_FRP, life_w, 'kv-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        
-        xtxt2 = xlabel('FRP strength reduction factor','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        ytxt2 = ylabel('Deisgn sevice life after strengthening (year)','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
+        struct_s = load(strcat(code, '_', 'shear', '+side_', fc, num2str(fccov), '.mat'));
+        struct_u = load(strcat(code, '_', 'shear', '+U_', fc, num2str(fccov), '.mat'));
+        struct_w = load(strcat(code, '_', 'shear', '+W_', fc, num2str(fccov), '.mat'));
+
+        data_struct_array = {struct_s, struct_u, struct_w}';
+        smp_RE = {};
+        mean_RE = {};
+        std_RE = {};
+        upper_RE = {};
+        lower_RE = {};
+        norm_RE = [];
+
+        for idata = 1:3
+            for ibeta = 1:4
+            [re_smpi, mean_REi, std_REi, upper_REi, lower_REi, norm_RE_yr30i, ...
+                norm_RE_yr40i, norm_RE_yr50i] = get_re_data( ...
+                data_struct_array{idata}, psi_f, code, beta_T(ibeta));
+                norm_RE{ibeta, idata} = norm_RE_yr50i;
+            end
+            smp_RE{end+1} = re_smpi;
+            mean_RE{end+1} = mean_REi;
+            std_RE{end+1} = std_REi;
+            upper_RE{end+1} = upper_REi;
+            lower_RE{end+1} = lower_REi;
+        end
+
+        %% post processing of shear
+        figs = {}; axs = {};
+        xlim_cell = {[0.1,2.0], [0.1,2.0], [0.1, 2.0]};
+        for ifig = 1:3
+            figs{end+1} = figure;
+            axs{end+1} = axes('Parent',figs{end},'FontSize',8,'FontName','Times New Roman');
+            box(axs{end},'on');
+            hold(axs{end},'all');
+            h1 = plot(psi_f, mean_RE{ifig}, 'k-', 'LineWidth', 1.5, 'MarkerFace', 'k', 'MarkerSize', 4);
+            h2 = plot(psi_f, upper_RE{ifig}, 'b-', 'LineWidth', 1, 'MarkerSize', 4);
+            h3 = plot(psi_f, lower_RE{ifig}, 'b-', 'LineWidth', 1, 'MarkerSize', 4);
+            X=[psi_f', fliplr(psi_f')];                %#create continuous x value array for plotting
+            Y=[upper_RE{ifig},fliplr(lower_RE{ifig})];              %#create continuous x value array for plotting
+            h4 = fill(X, Y, [0.99, 0.92, 0.8]);
+            uistack(h4,'bottom');
+            set(axs{end}, 'Layer', 'top')
+            legend([h1, h2, h3], {'mean', 'upper bound', 'load bound'})
+            xtxt = xlabel('FRP calibration factor, \phi_f','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+            ytxt = ylabel('Reliability index','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+
+            figs{end+1} = figure;
+            axs{end+1} = axes('Parent',figs{end},'FontSize',8,'FontName','Times New Roman');
+            box(axs{end},'on');
+            hold(axs{end},'all');
+            map = colormap('parula');
+            colors = map(floor(linspace(length(map), 1, 4)), :);
+            h1 = plot(psi_f, norm_RE{1, ifig}, 'o-', 'Color', colors(1,:), 'LineWidth', 1, ...
+                'MarkerFace', colors(1,:), 'MarkerEdge', colors(1,:), 'MarkerSize', 4);
+            h2 = plot(psi_f, norm_RE{2, ifig}, '^-', 'Color', colors(2,:), 'LineWidth', 1, ...
+                'MarkerFace', colors(2,:), 'MarkerEdge', colors(2,:), 'MarkerSize', 4);
+            h3 = plot(psi_f, norm_RE{3, ifig}, 'v-', 'Color', colors(3,:), 'LineWidth', 1, ...
+                'MarkerFace', colors(3,:), 'MarkerEdge', colors(3,:), 'MarkerSize', 4);
+            h4 = plot(psi_f, norm_RE{4, ifig}, 's-', 'Color', colors(4,:), 'LineWidth', 1, ...
+                'MarkerFace', colors(4,:), 'MarkerEdge', colors(4,:), 'MarkerSize', 4);
+            legend([h1, h2, h3, h4], {'\beta_T=3.0', '\beta_T=3.2', '\beta_T=3.5', '\beta_T=3.7'})
+            xtxt = xlabel('FRP calibration factor, \phi_f','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+            ytxt = ylabel('Mean distance from target','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+        end
     case 2
-        beam = 'flexure';
-        struct_ic = load(strcat('data_', code, '_', beam, '+IC_', fc, '_', num2str(fccov), '.mat'));
-        struct_rup = load(strcat('data_', code, '_', beam, '+rupture_', fc, '_', num2str(fccov), '.mat'));
-        life_ic = 50*log(normcdf(beta_T_50))./log(normcdf(struct_ic.mean_RE)); life_ic(life_ic>MAX_YR) = MAX_YR;
-        life_rup = 50*log(normcdf(beta_T_50))./log(normcdf(struct_rup.mean_RE)); life_rup(life_rup>MAX_YR) = MAX_YR;
+        struct_ic = load(strcat(code, '_', 'flexure', '+IC_', fc, num2str(fccov), '.mat'));
+        struct_rup = load(strcat(code, '_', 'flexure', '+rupture_', fc, num2str(fccov), '.mat'));
         
-        %% post processing of reliability analysis
-        figure1 = figure;
-        axes1 = axes('Parent',figure1,'FontSize',8,'FontName','Times New Roman');
-        box(axes1,'on');
-        hold(axes1,'all');
+        data_struct_array = {struct_ic, struct_rup}';
+        smp_RE = {};
+        mean_RE = {};
+        std_RE = {};
+        upper_RE = {};
+        lower_RE = {};
+        norm_RE_yr30 = {};
+        norm_RE_yr40 = {};
+        norm_RE_yr50 = {};
         
-        hic1 = plot(FACTOR_FRP, struct_ic.mean_RE, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hrup1 = plot(FACTOR_FRP, struct_rup.mean_RE, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        
-        xtxt1 = xlabel('FRP strength reduction factor','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        ytxt1 = ylabel('Mean reliability index, $\bar{\beta}$','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        
-        figure2 = figure;
-        axes2 = axes('Parent',figure2,'FontSize',8,'FontName','Times New Roman');
-        box(axes2,'on');
-        hold(axes2,'all');
-        
-        hic2 = plot(FACTOR_FRP, life_ic, 'ko-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        hrup2 = plot(FACTOR_FRP, life_rup, 'k^-', 'LineWidth', 1, 'MarkerFace', 'k', 'MarkerSize', 4);
-        
-        xtxt2 = xlabel('FRP strength reduction factor','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
-        ytxt2 = ylabel('Deisgn sevice life after strengthening (year)','FontSize',8,...
-            'FontName','Times New Roman', 'Interpreter','latex');
+        for idata = 1:2
+            [re_smpi, mean_REi, std_REi, upper_REi, lower_REi, norm_RE_yr30i, ...
+                norm_RE_yr40i, norm_RE_yr50i] = get_re_data( ...
+                data_struct_array{idata}, psi_f, code, beta_T_50);
+            smp_RE{end+1} = re_smpi;
+            mean_RE{end+1} = mean_REi;
+            std_RE{end+1} = std_REi;
+            upper_RE{end+1} = upper_REi;
+            lower_RE{end+1} = lower_REi;
+            norm_RE_yr30{end+1} = norm_RE_yr30i;
+            norm_RE_yr40{end+1} = norm_RE_yr40i;
+            norm_RE_yr50{end+1} = norm_RE_yr50i;
+        end
+
+        %% post processing of shear
+        figs = {}; axs = {};
+        xlim_cell = {[0.1,2.0], [0.1,2.0]};
+        for ifig = 1:2
+            figs{end+1} = figure;
+            axs{end+1} = axes('Parent',figs{end},'FontSize',8,'FontName','Times New Roman');
+            box(axs{end},'on');
+            hold(axs{end},'all');
+            h1 = plot(psi_f, mean_RE{ifig}, 'k-', 'LineWidth', 1.5, 'MarkerFace', 'k', 'MarkerSize', 4);
+            h2 = plot(psi_f, upper_RE{ifig}, 'b-', 'LineWidth', 1, 'MarkerSize', 4);
+            h3 = plot(psi_f, lower_RE{ifig}, 'b-', 'LineWidth', 1, 'MarkerSize', 4);
+            X=[psi_f', fliplr(psi_f')];                %#create continuous x value array for plotting
+            Y=[upper_RE{ifig},fliplr(lower_RE{ifig})];              %#create continuous x value array for plotting
+            h4 = fill(X, Y, [0.99, 0.92, 0.8]);
+            uistack(h4,'bottom');
+            set(axs{end}, 'Layer', 'top')
+            legend([h1, h2, h3], {'mean', 'upper bound', 'load bound'})
+            xtxt = xlabel('FRP calibration factor, \phi_f','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+            ytxt = ylabel('Reliability index','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+
+            figs{end+1} = figure;
+            axs{end+1} = axes('Parent',figs{end},'FontSize',8,'FontName','Times New Roman');
+            box(axs{end},'on');
+            hold(axs{end},'all');
+            plot(psi_f, norm_RE_yr30{ifig}, 'ko-', 'LineWidth', 1.5, 'MarkerFace', 'k', 'MarkerSize', 4);
+            plot(psi_f, norm_RE_yr40{ifig}, 'b^-', 'LineWidth', 1.5, 'MarkerSize', 4);
+            plot(psi_f, norm_RE_yr50{ifig}, 'rv-', 'LineWidth', 1.5, 'MarkerSize', 4);
+            xtxt = xlabel('FRP calibration factor, \phi_f','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+            ytxt = ylabel('Mean distance from target','FontSize',8,...
+                'FontName','Times New Roman', 'Interpreter','tex');
+            
+            switch code
+                case 'gb'
+                    xlim(axs{end},xlim_cell{ifig});
+                    xlim(axs{end-1},xlim_cell{ifig});
+                otherwise
+            end  
+        end
 end     
 
 %% change the following properties from figure inspector
-set(figure1, 'Units','inches', 'Position', [0, 0, 3.5, 2.625]);
-set(axes1, 'Units', 'normalized', 'Position', [0.16, 0.15, 0.75, 0.79]);
-% savefig(figure1, strcat('../figures/HKandACInew/re_', code, '_', fc, '.fig'));
-% set(xtxt1, 'Units', 'normalized', 'Position', [-0.1, 0.5, 0]);
-% set(ytxt1, 'Units', 'normalized', 'Position', [0.5, -0.1, 0]);
-
-set(figure2, 'Units','inches', 'Position', [0, 0, 3.5, 2.625]);
-set(axes2, 'Units', 'normalized', 'Position', [0.16, 0.15, 0.75, 0.79]);
-% savefig(figure2, strcat('../figures/HKandACInew/life_', code, '_', fc, '.fig'))
-% set(xtxt2, 'Units', 'normalized', 'Position', [-0.1, 0.5, 0]);
-% set(ytxt2, 'Units', 'normalized', 'Position', [0.5, -0.1, 0]);
+nfig = length(figs);
+for ifig = 1:nfig
+    fig = figs{ifig};
+    ax = axs{ifig};
+    set(fig, 'Units','inches', 'Position', [0, 0, 3.5, 2.625]);
+    set(ax, 'Units', 'normalized', 'Position', [0.16, 0.15, 0.75, 0.79]);
+    % savefig(figure1, strcat('../figures/HKandACInew/re_', code, '_', fc, '.fig'));
+    % set(xtxt1, 'Units', 'normalized', 'Position', [-0.1, 0.5, 0]);
+    % set(ytxt1, 'Units', 'normalized', 'Position', [0.5, -0.1, 0]);
+end
